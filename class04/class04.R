18fa2d0cb7f06c877a4569e21099c34e2d8b42a4
Aigerim Kuanbay <kuanbai.aigerim93@gmail.com>
2024-02-29 05:46:01 +0600

    Add class4, 6, 7, 8 to github

---
+++ class04/class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "lab class 04"
+#' author: "Aigerim Kuanbay"
+#' ---
+
+# This is just a text file
+x <- 1:50
+
+plot(x,sin(x))
+plot(x,sin(x), typ="l")
+plot(x,sin(x), typ="l", col="blue")
+plot(x,sin(x), typ="l", col="blue", lwd=2, xlab="hello")
